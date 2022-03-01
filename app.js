3d1e352f9a2201e34dd14805a48bddb1958d3bb9
Fahad Guled <fahad.guled@yahoo.com>
2022-02-28 21:44:24 -0800

    created an object for every new recipe and set each property equal to appropriate food attribute. Also stored food info into local storage so on the refresh, it will store the new recipe generated into the local storage.

--- app.js
+++ app.js
@@ -6,15 +6,39 @@ const steps = document.getElementById('steps');
 const foodImage = document.getElementById('food-pic');
 const demo = document.getElementById('demo');
 
-// object for each different food that contains all the information required//
-let food = {
-    foodImage: null,
-    foodName: 'burger',
-    ingredients: 'bun patty cheese meat',
-    steps: 'here are the steps',
-    youtubeVideo: 'button'
-};
-
-localStorage.setItem('food', JSON.stringify(food));
 
 
+async function randomRecipe() {
+    const response = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
+    const randomMeal = await response.json();
+    console.log(randomMeal);
+    foodImage.src = randomMeal.meals[0].strMealThumb;
+    foodName.innerHTML = "Food: " + randomMeal.meals[0].strMeal;
+    steps.innerHTML = "Steps: " + randomMeal.meals[0].strInstructions;
+    console.log(randomMeal.meals[0].strYoutube)
+    youtubeVideo.href = randomMeal.meals[0].strYoutube;
+    ingredients.innerHTML = randomMeal.meals[0].strIngredient1 + ", " + randomMeal.meals[0].strIngredient2;
+    // object for each different food that contains all the information required//
+    let food = {
+        foodImage: foodImage.src,
+        foodName: foodName.innerHTML,
+        ingredients: ingredients.innerHTML,
+        steps: steps.innerHTML,
+        youtubeVideo: youtubeVideo.href,
+    };
+
+    console.log(food)
+    //Set to local storage//
+    localStorage.setItem('food', JSON.stringify(food));
+    
+    let getFood = JSON.parse(localStorage.getItem('todo'))
+    console.log(getFood)
+}
+
+//New Recipe Button //
+newRecipe.addEventListener('click', () => {
+    randomRecipe();
+    
+})
+
+//randomMeal.meals[0].//
